699b436eafad5296fb871c0e3998b2191b65b4b6
RoboRace <roborace@sirious.nl>
2025-10-04 15:36:14 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,25 +2,13 @@
 namespace myGlobals {
     let _lives = 3
 
-    /**
-     * Get current lives
-     */
-    //% block="lives" 
-    //% block.loc.category="Variables"
-    //% blockNamespace="Variables"
-    //% blockSetVariable="lives"
+    //% block="lives"
     export function getLives(): number {
         return _lives
     }
 
-    /**
-     * Set lives to a value
-     */
     //% block="set lives to %value"
-    //% block.loc.category="Variables"
-    //% blockNamespace="Variables"
     export function setLives(value: number): void {
         _lives = value
     }
 }
-
