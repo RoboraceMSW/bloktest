f2032e210b0ec3b61ada696543b91d452cfd590a
RoboRace <roborace@sirious.nl>
2025-10-04 15:27:12 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,26 @@
+//% color="#AA278D" icon="\uf005"
+namespace myGlobals {
+    let _lives = 3
+
+    /**
+     * Get current lives
+     */
+    //% block="lives" 
+    //% block.loc.category="Variables"
+    //% blockNamespace="Variables"
+    //% blockSetVariable="lives"
+    export function getLives(): number {
+        return _lives
+    }
+
+    /**
+     * Set lives to a value
+     */
+    //% block="set lives to %value"
+    //% block.loc.category="Variables"
+    //% blockNamespace="Variables"
+    export function setLives(value: number): void {
+        _lives = value
+    }
+}
 
